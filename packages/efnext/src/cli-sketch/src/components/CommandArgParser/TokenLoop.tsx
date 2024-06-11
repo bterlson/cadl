2d8f34a687d5df6443394ad860d7514802d81c53
Brian Terlson <brian.terlson@microsoft.com>
2024-06-11 08:27:00 -0700

    add forgotten file

---
+++ packages/efnext/src/cli-sketch/src/components/CommandArgParser/TokenLoop.tsx
@@ -0,0 +1,29 @@
+import { code } from "#typespec/emitter/core";
+import { useCommand } from "./CommandArgParser.js";
+import { OptionTokenHandler } from "./OptionTokenHandler.js";
+import { PositionalTokenHandler } from "./PositionalTokenHandler.js";
+
+export interface TokenLoopProps {}
+export function TokenLoop({}: TokenLoopProps) {
+  const { command, options } = useCommand();
+  const optionTokenHandlers = Array.from(options.entries()).map(([option, path]) => (
+    <OptionTokenHandler option={option} path={path} />
+  ));
+
+  return code`
+    for (const token of tokens) {
+      if (token.kind === "positional") {
+        ${(<PositionalTokenHandler />)}
+      } else if (token.kind === "option") {
+        switch (token.name) {
+          case "h":
+          case "help":
+            ${command.name}Help();
+            return;
+          ${optionTokenHandlers}
+        }
+      }
+    }
+    (handler.${command.name} as any)(... marshalledArgs);
+  `;
+}
